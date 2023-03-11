ac87e82a9a8e18ba461ec00783b908e842277402
Melanie Ta <nngoc2701@gmail.com>
2023-03-11 14:44:16 -0800

    Modify backend to allow request from local frontend

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,64 @@
-import React, {useState, useEffect} from 'react'
+import { useState } from "react";
+import axios from "axios";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  // new line start
+  const [profileData, setProfileData] = useState(null);
 
-  const [data, setData] = useState([])
-  
-  useEffect(() => {
-    fetch('/').then(
-      res => res.json()
-    ).then(
-      data => {
-        setData(data)
-      console.log(data)
-      }
-    )
-  }, [])
+  function getData() {
+    axios({
+      method: "GET",
+      url: "http://127.0.0.1:8000/",
+    })
+      .then((response) => {
+        const res = response.data;
+        console.log(res);
+        setProfileData({
+          profile_name: res.name,
+          about_me: res.about,
+        });
+      })
+      .catch((error) => {
+        if (error.response) {
+          console.log(error.response);
+          console.log(error.response.status);
+          console.log(error.response.headers);
+        }
+      });
+  }
+  //end of new line
+
   return (
-    <div></div>
-  )
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+
+        {/* new line start*/}
+        <p>To get your profile details: </p>
+        <button onClick={getData}>Click me</button>
+        {profileData && (
+          <div>
+            <p>Profile name: {profileData.profile_name}</p>
+            <p>About me: {profileData.about_me}</p>
+          </div>
+        )}
+        {/* end of new line */}
+      </header>
+    </div>
+  );
 }
 
-export default App
+export default App;
--- flask-server/server.py
+++ flask-server/server.py
@@ -1,12 +1,17 @@
 from flask import Flask
+from flask_cors import CORS
 
 app = Flask(__name__)
+cors = CORS(app, resources={r"/*": {"origins": "*"}})
 
 # API routes and endpoints to get data
 @app.route('/')
 def index():
-	return {"members": ["John", "Paul", "George", "Ringo"]}
-
+	response_body = {
+		"name": "FraudAI",
+		"about": "Hello, this is fraudAI"
+	}
+	return response_body
 
 if __name__ == '__main__':
 	app.run(debug=True)
