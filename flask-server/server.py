a7d5ca48237210b6faac3bc57e0859896226f406
Cindy Liu <cindy.liu@vidyard.com>
2023-03-11 13:52:26 -0800

    add react client side

--- flask-server/server.py
+++ flask-server/server.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 # API routes and endpoints to get data
 @app.route('/')
 def index():
-	return 'Hello, World!'
+	return {"members": ["John", "Paul", "George", "Ringo"]}
 
 
 if __name__ == '__main__':
