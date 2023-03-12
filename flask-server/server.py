978bda1882b3291f88f54fd6dd3e16ece08a8ea2
Melanie Ta <nngoc2701@gmail.com>
2023-03-11 18:06:02 -0800

    Remove env print in server. Update client package

--- flask-server/server.py
+++ flask-server/server.py
@@ -1,5 +1,3 @@
-import os
-from dotenv import load_dotenv
 from flask import Flask
 from flask_cors import CORS
 
@@ -10,11 +8,6 @@ cors = CORS(app, resources={r"/*": {"origins": "*"}})
 # API routes and endpoints to get data
 @app.route('/')
 def index():
-	print(os.getenv('COHERE_API_KEY'))
-	#print everything
-	for key, value in os.environ.items():
-		print('{}: {}'.format(key, value))
-
 	response_body = {
 		"name": "FraudAI",
 		"about": "Hello, this is fraudAI"
