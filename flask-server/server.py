efd2272b3e89ad9fba7ed60ceec3b6862f2840e1
Cindy Liu <cindy.liu@vidyard.com>
2023-03-11 20:23:17 -0800

    add spam post path

--- flask-server/server.py
+++ flask-server/server.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, redirect, url_for, request, render_template
 from flask_cors import CORS
 
 app = Flask(__name__)
@@ -7,11 +7,14 @@ cors = CORS(app, resources={r"/*": {"origins": "*"}})
 # API routes and endpoints to get data
 @app.route('/')
 def index():
-	response_body = {
-		"name": "FraudAI",
-		"about": "Hello, this is fraudAI"
-	}
-	return response_body
+	return render_template('index.html')
+
+@app.route("/spam", methods=["POST"])
+def spam():
+	text = request.form['input_text']
+	processed_text = text.upper()
+
+	return processed_text()
 
 if __name__ == '__main__':
 	app.run(debug=True)
