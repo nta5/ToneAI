17fe197aa89e7c95144ea680c4516c25f5250540
Melanie Ta <nngoc2701@gmail.com>
2023-03-11 18:19:28 -0800

    Try remove host name

--- client/src/App.js
+++ client/src/App.js
@@ -34,17 +34,13 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <form>
+          <label>
+            Name:
+            <input type="text" name="name" />
+          </label>
+          <input type="submit" value="Submit" />
+        </form>
 
         {/* new line start*/}
         <p>To get your profile details: </p>
