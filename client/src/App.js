8efa328377fd72f198a1cf96903047f1838ebab5
Cindy Liu <cindy.liu@vidyard.com>
2023-03-12 10:25:17 -0700

    add navbar

--- client/src/App.js
+++ client/src/App.js
@@ -75,6 +75,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <h1 id="navbar">ToneAI</h1>
         <form onSubmit={getData}>
           <textarea
             placeholder="Enter your text here"
