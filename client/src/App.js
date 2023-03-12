f5d333f08fb340877ec8350a8d2bb03550cbd5ac
Melanie Ta <nngoc2701@gmail.com>
2023-03-12 11:15:40 -0700

    Update UI

--- client/src/App.js
+++ client/src/App.js
@@ -44,8 +44,8 @@ function App() {
     e.preventDefault();
     axios({
       method: "post",
-      url: "http://127.0.0.1:8000/spam",
-      // url: "https://toneapi.onrender.com/spam",
+      // url: "http://127.0.0.1:8000/spam",
+      url: "https://toneapi.onrender.com/spam",
       data: JSON.stringify(analysisForm),
       headers: { "Content-Type": "application/json" },
     })
@@ -129,49 +129,51 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={ToneAI} id="navbar" alt="ToneAI" />
-        <h1 id="navbar">ToneAI</h1>
         <Grid container spacing={3}>
-          <Grid item xs={6} p={2} id="form-data"></Grid>
-          <form onSubmit={getData}>
-            <textarea
-              placeholder="Enter your text here"
-              type="text"
-              name="paragraph"
-              value={analysisForm.paragraph}
-              onChange={(e) => onChangeHandler(e.target)}
-              cols="50"
-              rows="20"
-            />
-            <br />
-            <div className="checkbox-container">
-              <ul className="option-list">
-                {scanOption.map((name, index) => {
-                  return (
-                    <li key={index}>
-                      <div className="list-item">
-                        <input
-                          type="checkbox"
-                          id={`custom-checkbox-${index}`}
-                          name={name}
-                          value={name}
-                          checked={checkedState[index]}
-                          onChange={() => handleOnChange(index)}
-                        />
-                        <label htmlFor={`custom-checkbox-${index}`}>
-                          {name}
-                        </label>
-                      </div>
-                    </li>
-                  );
-                })}
-              </ul>
-            </div>
-            <br />
-            <input type="submit" id="submit-button" value="Submit" />
-          </form>
+          <Grid item xs={12} p={2} id="navbar">
+            <img src={ToneAI} id="navbar-image" alt="ToneAI" />
+          </Grid>
+          <Grid item xs={6} p={2} id="form-data">
+            <form onSubmit={getData}>
+              <textarea
+                placeholder="Enter your text here"
+                type="text"
+                name="paragraph"
+                value={analysisForm.paragraph}
+                onChange={(e) => onChangeHandler(e.target)}
+                cols="50"
+                rows="20"
+              />
+              <br />
+              <div className="checkbox-container">
+                <ul className="option-list">
+                  {scanOption.map((name, index) => {
+                    return (
+                      <li key={index}>
+                        <div className="list-item">
+                          <input
+                            type="checkbox"
+                            id={`custom-checkbox-${index}`}
+                            name={name}
+                            value={name}
+                            checked={checkedState[index]}
+                            onChange={() => handleOnChange(index)}
+                          />
+                          <label htmlFor={`custom-checkbox-${index}`}>
+                            {name}
+                          </label>
+                        </div>
+                      </li>
+                    );
+                  })}
+                </ul>
+              </div>
+              <br />
+              <input type="submit" id="submit-button" value="Submit" />
+            </form>{" "}
+          </Grid>
 
-          <Grid item xs={6} p={2}>
+          <Grid item xs={6} p={2} id="result">
             {/* new line start*/}
             {result && (
               <div>
